7d7b4efa17b6910983f6e62778e5cd06b22368c4
Yan Jie Tay <tayyanjie@gmail.com>
2022-09-01 16:13:57 +0800

    Added docstrings

--- app/app.py
+++ app/app.py
@@ -1,21 +1,27 @@
+'''
+Flask app to allow for upload of csv to generate predictions returned in CSV format.
+'''
 import os
 import joblib
 import pandas as pd
 from flask import Flask, request, make_response, render_template
 
+
+
 app = Flask(__name__)
 path = os.path.dirname(os.path.realpath(__file__))
 model = joblib.load(os.path.join(path, "../models/LogisticRegressionModel.joblib"))
-print("loaded model")
 
 
 @app.route("/")
 def form():
+    """Generates Form to upload csv for prediction"""
     return render_template("index.html")
 
 
 @app.route("/", methods=["POST"])
 def predict():
+    """Conduct prediction and allow for download of resulting CSV"""
     file = request.files.get("file")
     data = pd.read_csv(file)
     res = model.predict(data)
