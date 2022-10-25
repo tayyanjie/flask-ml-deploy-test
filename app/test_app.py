8fa55c3292c8a7846c37e511d671f0c2b3d11906
Yan Jie Tay <tayyanjie@gmail.com>
2022-10-25 18:05:46 +0800

    Fix linting issues

--- app/test_app.py
+++ app/test_app.py
@@ -1,17 +1,21 @@
+''' Test Flask App '''
+import os
 from app import app
-import pytest
-import os 
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
+
 def test_index_route():
+    """ Tests that home page works """
     response = app.test_client().get('/')
     assert response.status_code == 200
     assert b"Upload your CSV file" in response.data
 
 def test_upload_form():
-    f = open(os.path.join(dir_path, "test_data.csv"), "rb")
+    """ Tests uploading of csv file for predictions """
+    file = open(os.path.join(dir_path, "test_data.csv"), "rb") # pylint: disable=consider-using-with
     response = app.test_client().post("/",
-            data={"file": f})
+            data={"file": file})
     assert response.status_code == 200
     assert response.headers["Content-Disposition"] == "attachment;filename=predictions.csv"
     assert response.headers["Content-Type"] == "text/csv"
